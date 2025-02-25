40ccdce5fd51d6f4f7e62380d597485a912fde61
Rodgers31 <otienor986@gmail.com>
2025-02-24 23:00:01 -0600

    Created paddle

---
+++ main.py
@@ -0,0 +1,46 @@
+from turtle import Turtle, Screen
+from paddles import Paddle
+
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong Game")
+
+paddle = Turtle()
+paddle.shape("square")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350, 0)
+paddle.color("white")
+
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+
+
+
+
+
+
+
+screen.exitonclick()
+
+
+
+
+
+
+
---
+++ paddles.py
@@ -0,0 +1,13 @@
+from turtle import Turtle
+
+
+class Paddle:
+
+    def __init__(self):
+        self.move = True
+
+    def up(self):
+        pass
+
+    def down(self):
+        pass
